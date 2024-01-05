b85d1fb64e2320e677ae380ee797d1c106f07665
USAMI Kenta <tadsan@zonu.me>
2024-01-06 05:59:44 +0900

    Add datetime-format-test-format

--- datetime-format-test.el
+++ datetime-format-test.el
@@ -33,6 +33,34 @@
 ;;; Code:
 (require 'ert)
 (require 'datetime-format)
+(require 'timecop)
+
+(defvar datetime-format-original-tz (getenv "TZ"))
+
+(defmacro datetime-format--map (list function)
+  "Apply FUNCTION to each element of LIST.
+This macro helps with expression expansion at compile time."
+  (declare (indent 1))
+  (let ((sequence (eval list)))
+    `(prog1 (quote ,sequence)
+       ,@(mapcar function sequence))))
+
+(ert-deftest datetime-format-test-format ()
+  (setenv "TZ" "-9:30") ;; Australia/Darwin
+  (datetime-format--map '(("2009-02-14T09:01:30+09:30" (datetime-format 'atom))
+           ("2009-02-14T09:01:30+09:30" (datetime-format 'atom (current-time)))
+           ("1970-01-01T09:30:00+09:30" (datetime-format 'atom 0))
+           ("1970-01-01T00:00:00+00:00" (datetime-format 'atom 0 :timezone "UTC"))
+           ("2015-01-12T02:31:11+09:30" (datetime-format 'atom "2015-01-12 02:01:11"))
+           ("2015-01-12T02:01:11+02:00" (datetime-format 'atom "2015-01-12 02:01:11"
+                                                         :timezone "Europe/Kiev"))
+           ("2015-01-11T17:01:11Z" (datetime-format 'atom-utc "2015-01-12 02:01:11"))
+           ("2009-02-13T18:31:30-05:00" (datetime-format 'atom nil
+                                                         :timezone "America/New_York")))
+    (lambda (pair)
+      (cl-destructuring-bind (expected expr) pair
+        `(timecop 1234567890 (should (string= ,expected (eval ,expr)))))))
+  (setenv "TZ" datetime-format-original-tz))
 
 (provide 'datetime-format-test)
 ;;; datetime-format-test.el ends here
